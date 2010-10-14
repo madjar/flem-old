8e8bcee7446709a25ec41a786908ef523b612b73
Georges Dubus <georges.dubus@compiletoi.net>
2010-10-15 00:06:52 +0200

    The model is here.

---
+++ food/admin.py
@@ -0,0 +1,8 @@
+from food.models import Section, Category, Ingredient, Recipe, Containment
+from django.contrib import admin
+
+admin.site.register(Section)
+admin.site.register(Category)
+admin.site.register(Ingredient)
+admin.site.register(Recipe)
+admin.site.register(Containment)
---
+++ food/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+# Create your models here.
+
+class Section(models.Model):
+    """Where you can find it in your supermarket"""
+    name = models.CharField(max_length=64)
+    position = models.IntegerField()
+
+    def __unicode__(self):
+        return self.name
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=64)
+    unit = models.CharField(max_length=64)
+    is_food = models.BooleanField()
+    section = models.ForeignKey(Section)
+
+    def __unicode__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'caterogy'
+        verbose_name_plural = 'categories'
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    category = models.ForeignKey(Category)
+    ingredients = models.ManyToManyField(Ingredient, through='Containment')
+
+    def __unicode__(self):
+        return self.name
+
+class Containment(models.Model):
+    recipe = models.ForeignKey(Recipe)
+    ingredient = models.ForeignKey(Ingredient)
+    amount = models.FloatField()
