9e60d9492e846e72d14c02d0c6b0bf00a6ec7c83
Georges Dubus <georges.dubus@compiletoi.net>
2010-11-14 20:08:19 +0100

    We can now make the planning using some javascript.

--- planning/urls.py
+++ planning/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('planning.views',
     url(r'^(?P<year_from>\d{4})/(?P<month_from>\d{2})/(?P<day_from>\d+)/'
         r'(?P<year_to>\d{4})/(?P<month_to>\d{2})/(?P<day_to>\d+)/$', 'range'),
+    url(r'^update/$', 'update'),
 
 )
--- planning/views.py
+++ planning/views.py
@@ -1,8 +1,12 @@
 from planning.models import Meal
+from food.models import Recipe
 
 from django.shortcuts import render_to_response
+from django.http import HttpResponse
+from django.utils import simplejson
 
 import datetime
+
 
 def range(request, year_from, month_from, day_from, year_to, month_to, day_to):
     date_from = datetime.date(int(year_from), int(month_from), int(day_from))
@@ -11,3 +15,20 @@ def range(request, year_from, month_from, day_from, year_to, month_to, day_to):
     meals = Meal.objects.filter(date__range=(date_from, date_to))
 
     return render_to_response('planning/range.html', {'meals': meals, 'from': date_from, 'to': date_to})
+
+
+def update(request):
+    if request.is_ajax():
+        new_planning = simplejson.loads(request.POST['new_planning'])
+
+        meals = Meal.objects.filter(id__in=new_planning.keys()).only('id').all()
+        Meal.recipes.through.objects.filter(meal__id__in=new_planning.keys()).delete()
+        for meal in meals:
+            recipe_ids = new_planning[str(meal.id)]
+            recipes = Recipe.objects.filter(id__in=recipe_ids).only('id').all()
+            for recipe in recipes:
+                Meal.recipes.through(meal=meal, recipe=recipe).save()
+
+        return HttpResponse("OK", mimetype="text/plain")
+    else:
+        return HttpResponse(status=400)
