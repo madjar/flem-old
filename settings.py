910f84427f944cbe1f3671831f2b0db26c15be54
Georges Dubus <georges.dubus@compiletoi.net>
2010-12-20 15:24:37 +0100

    Deactivate the redirection prevention of the toolbar, because it makes the admon site unbearable.

--- settings.py
+++ settings.py
@@ -106,5 +106,6 @@ try:
     import debug_toolbar
     MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
     INSTALLED_APPS += ('debug_toolbar',)
+    DEBUG_TOOLBAR_CONFIG = {'INTERCEPT_REDIRECTS': False}
 except ImportError:
     pass
